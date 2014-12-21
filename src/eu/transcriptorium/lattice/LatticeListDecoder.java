375c58ffe127cf814ee1bd0c28c651662694029f
jessededoes <jesse.dedoes@inl.nl>
2014-12-21 13:34:48 +0100

    last commit: zo dec 21 13:34:48 CET 2014

--- src/eu/transcriptorium/lattice/LatticeListDecoder.java
+++ src/eu/transcriptorium/lattice/LatticeListDecoder.java
@@ -1,6 +1,9 @@
 package eu.transcriptorium.lattice;
 import java.util.*;
 
+import edu.berkeley.nlp.lm.NgramLanguageModel;
+import eu.transcriptorium.lm.VariantLexicon;
+
 public class LatticeListDecoder 
 {
 	//LatticeDecoder decoder;
@@ -9,31 +12,41 @@ public class LatticeListDecoder
 	//List<Integer> partialPathLengths = new ArrayList<Integer>();
 
 	List<NodePathInfo> endPoints = new ArrayList<NodePathInfo>();
+	NgramLanguageModel lm;
+	VariantLexicon variantLexicon;
 	
-	public LatticeListDecoder(LatticeDecoder d) // nee dus, je moet er meerdere hebben
+	public LatticeListDecoder(NgramLanguageModel lm, VariantLexicon v) // nee dus, je moet er meerdere hebben
 	{
 		//this.decoder = d;
+		this.lm = lm;
+		this.variantLexicon = v;
 	}
 
-	public void decode(List<Lattice> lattices)
+	public List<String> decode(List<Lattice> lattices)
 	{
+		List<String> decodingResult = null;
 		for (int i=0; i < lattices.size(); i++)
 		{
 			LatticeDecoder decoder = new LatticeDecoder();
 			decoders.add(decoder);
+			decoder.setLanguageModel(lm);
+			decoder.setVariantLexicon(variantLexicon);
+			
 			if (i==0)
 			{
-				decoder.decode(lattices.get(0));
+				decodingResult = decoder.decode(lattices.get(0));
 				endPoints.add(decoder.getLastPathInfo());
 			}
 			else
 			{
 				NodePathInfo prev = decoder.getLastPathInfo();
 				decoder.setPreviousNodeinfo(prev);
-				decoder.decode(lattices.get(i));
+				decodingResult = decoder.decode(lattices.get(i));
 				endPoints.add(decoder.getLastPathInfo());
 			}
 		}
+		return decodingResult;
+		// now use the last decoder to get out everything (in fact, each step traces back to the first?)
 		// en dan weer de hele rompslomp om het beste pad er uit te peuteren...
 		// zal best wel gedoe zijn enzo.... BOE BOE
 	}

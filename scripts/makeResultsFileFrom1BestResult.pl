99deb80e9eaeac44491b7b22e107ab7e60ea8910
Jesse de Does <jesse.dedoes@inl.nl>
2014-12-22 09:50:31 +0100

    last commit: Mon Dec 22 09:50:30 CET 2014

--- scripts/makeResultsFileFrom1BestResult.pl
+++ scripts/makeResultsFileFrom1BestResult.pl
@@ -1,16 +1,32 @@
 # 115_009_002_02_16.fea <s> AS WELL AS FROM SUFFERING FROM THE INCLEMENCY OF </s>
 
+use Getopt::Std;
+our $opt_i=0;
+getopts('i');
+my $insens=$opt_i;
+
 while(<>)
 {
-  if (/^(.*)\.lattice.*<s>(.*)<\/s>(.*)/)
+  if (/^(.*)\.(lattice|fea).*<s>(.*)<\/s>(.*)/)
   {
-    my ($line,$text,$extra) = ($1,$2,$3); 
+    my ($line,$text,$extra) = ($1,$3,$4); 
     $line =~ s/.*\///;
     my $ref = "./data/Transcription/$line.txt";
     my $reftxt;
     open(R,$ref);
     while(<R>) { chomp(); $reftxt .=  $_; };
-    print "$reftxt \$  $text # $extra\n";
+    if ($insens)
+    {
+       $reftxt = uc $reftxt;
+       $text = uc $text;
+    }
+    if ($extra)
+    {
+      print "$reftxt \$  $text # $extra\n";
+    } else
+    {
+       print "$reftxt \$  $text\n";
+    }
     close(R);
   }
 }

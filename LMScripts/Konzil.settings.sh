274fb50cc27a3e4935fd7e0a57f5f01130c5fe55
Jesse de Does <jesse.dedoes@inl.nl>
2015-10-13 12:04:28 +0200

    last commit: Tue Oct 13 12:04:26 CEST 2015

---
+++ LMScripts/Konzil.settings.sh
@@ -0,0 +1,18 @@
+KONZIL=/mnt/Projecten/transcriptorium/Data/Corpora/German/Konzil/ExtraText
+CORPUS=$KONZIL/KonzilExtra.txt
+OUTPUT=$KONZIL/LM
+CLASS_CHARSET=eu.transcriptorium.lm.charsets.SimpleTokenization
+#CLASS_CHARSET=eu.transcriptorium.lm.charsets.ProcessingForCharacterLM
+
+CHARSET=resources/CharacterSets/Konzil.chars.txt
+
+CUTOFF=0
+
+export CLASSPATH=./build/classes
+
+export SRILM_HOME=/home/jesse/Tools/srilm
+export SRILM_HOME=/mnt/Projecten/transcriptorium/Tools/SRILM
+export PATH=$SRILM_HOME/bin:/$SRILM_HOME/bin/i686-m64/:/usr/bin/:$PATH
+
+export HTK=/home/jesse/Tools/htk3-4-Atros/bin.linux/
+export HTK=/usr/local/bin

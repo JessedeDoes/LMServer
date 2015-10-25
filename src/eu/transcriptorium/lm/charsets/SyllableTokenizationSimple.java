8fdb4e6be36e7dcd730da1769eac9a3471fe5a6e
Jesse de Does <jesse.dedoes@inl.nl>
2015-10-25 09:57:03 +0100

    ..

---
+++ src/eu/transcriptorium/lm/charsets/SyllableTokenizationSimple.java
@@ -0,0 +1,59 @@
+package eu.transcriptorium.lm.charsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.transcriptorium.lm.CharacterSet;
+import eu.transcriptorium.util.StringUtils;
+
+/**
+ * Good syllable splitting does not really matter here..
+ * Ideally - split in such a way that LM is best....
+ * Or just preprocess corpus with syllable splitter
+ * @author jesse
+ *
+ */
+public class SyllableTokenizationSimple extends ProcessingForCharacterLMSimple
+{
+	public static String splitMark = "_"; // should be added to character set....
+	
+	@Override
+	public String[] splitInCharacters(String s)
+	{
+		s = unescapeWord(s);
+		String[] p = s.split(splitMark);
+		
+		for (int i=0; i < p.length; i++)
+		{
+			p[i] = escapeWord(p[i]);
+		}
+		return p;
+	}
+	
+	@Override
+	public void postInit()
+	{
+		super.postInit();
+		characterAccepted['_'] = true;
+	}
+	
+	public static void main(String[] args)
+	{
+		CharacterSet dat = new SyllableTokenizationSimple();
+		dat.setAcceptAll();
+		
+		String test = "Dogs are, in_deed, re_mar_ka_ble ani_mals";
+		for (String w: test.split("\\s+"))
+		{
+			String cleaned = dat.cleanWord(w);
+			//System.err.println("cleaned " + cleaned);
+			for (String tok: cleaned.split("\\s+"))
+			{
+				//System.err.println("Tok: " + tok);
+				String norm = dat.normalize(tok); 
+			    System.out.println(w + " " + tok +  " " + 
+			    		dat.normalize(tok) + " --> "  + StringUtils.join(dat.wordToModelNames(tok), " "));
+			}
+		}
+	}
+}

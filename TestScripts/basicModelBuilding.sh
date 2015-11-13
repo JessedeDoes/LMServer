ddca4c5d4865a11983b3de83605d856e7d772477
Jesse de Does <jesse.dedoes@inl.nl>
2015-11-13 17:03:36 +0100

    last commit: Fri Nov 13 17:03:33 CET 2015

---
+++ TestScripts/basicModelBuilding.sh
@@ -0,0 +1,16 @@
+source $1
+
+#rm -rf $OUTPUT/*
+
+source LMScripts/LMBuildingFunctions.sh
+
+
+TextAndLexicalProcessing2 $CORPUS $CLASS_CHARSET $CHARSET $CUTOFF $OUTPUT $ORDER
+
+LanguageModelingWithoutBigLM $OUTPUT/normalizedText.txt $OUTPUT/normalizedWordList.txt $OUTPUT/languageModel.lm $ORDER
+
+echo "start HBuild"
+
+HBuild -s '<s>' '</s>' -n $OUTPUT/languageModel.lm $OUTPUT/dictionary.txt $OUTPUT/latticeFile.txt 2>/tmp/hbuild.log
+
+cat /tmp/hbuild.log

143d18f3448bf31eafa5f728cfc2df173cf365f9
Jesse de Does <jesse.dedoes@inl.nl>
2015-12-20 15:32:29 +0100

    ..

---
+++ LMScripts/Meermanno/mnl_proza.settings.sh
@@ -0,0 +1,30 @@
+
+CLASS_CHARSET=eu.transcriptorium.lm.charsets.AlejandrosNewBenthamTokenization
+#CLASS_CHARSET=eu.transcriptorium.lm.charsets.SimpleTokenization
+SPECIAL_LABELS=/home/jesse/workspace/LMServer/resources/CharacterSets/special_labels.txt
+TRANSCRIPTION_DIR=/home/jesse/TUTORIAL-HTR/EXP-MEERMANNO/TRAIN/Transcriptions
+TRAINING_PARTITION=/home/jesse/TUTORIAL-HTR/EXP-MEERMANNO/TRAIN/train.lst
+TRAIN_LINES=/home/jesse/TUTORIAL-HTR/EXP-MEERMANNO/TRAIN/Train-Lines.lst.x
+BASIC_TRAINING_SOURCE=/home/jesse/TUTORIAL-HTR/EXP-MEERMANNO/TRAIN/LM/TC
+
+
+MNLDIR=/media/jesse/Data/tranScriptorium/NederlandseData/MNL-plaintext/Corpora
+######### 
+CORPUS=$MNLDIR/CDROM-proza.txt
+OUTPUT=/home/jesse/TUTORIAL-HTR/EXP-MEERMANNO/TRAIN/LM/MNL-PROZA
+CHARSET=$BASIC_TRAINING_SOURCE/charset.txt
+CUTOFF=1
+
+
+
+
+### path and classpath settings
+
+export CLASSPATH=/home/jesse/workspace/LMServer/build/classes
+
+export SRILM_HOME=/home/jesse/Tools/srilm
+#export SRILM_HOME=/mnt/Projecten/transcriptorium/Tools/SRILM
+export PATH=$SRILM_HOME/bin:/$SRILM_HOME/bin/i686-m64/:/usr/bin/:$PATH
+
+export HTK=/home/jesse/Tools/htk3-4-Atros/bin.linux/
+#export HTK=/usr/local/bin
